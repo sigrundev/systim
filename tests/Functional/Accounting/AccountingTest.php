4dda6dc3211a289a50e97c0e789465b3d5831446
Marek Ognicki <marek@sigrun.eu>
2019-09-23 12:17:43 +0200

    Added Accounting tests

--- tests/Functional/Accounting/AccountingTest.php
+++ tests/Functional/Accounting/AccountingTest.php
@@ -6,5 +6,19 @@ use Systim\Tests\FunctionalSystimTestCase;
 
 class AccountingTest extends FunctionalSystimTestCase
 {
+    public function testGenerateTaxInformation() {
 
+    }
+
+    public function testGenerateBalanceSheet() {
+
+    }
+
+    public function testGenerateProfitAndLossAcount() {
+
+    }
+
+    public function testListAccountingYears() {
+
+    }
 }
